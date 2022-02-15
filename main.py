a7bec4f41ee74337713946684d0f2e119fae2252
Udit <uditmishra.um@gmail.com>
2022-02-15 23:05:33 +0530

    Turtle random walk with random colors

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import turtle
 from turtle import Turtle, Screen
 import random
 
@@ -8,6 +9,8 @@ squirtle.get_shapepoly()
 squirtle.speed("fastest")
 screen = Screen()
 
+turtle.colormode(255)
+
 # ((50, -20), (30, 20), (-50, 20), (-30, -20))
 # for _ in range(100):
 #     squirtle.forward(500/100)
@@ -17,17 +20,24 @@ screen = Screen()
 #     # squirtle.right(90)
 
 no_of_sides = 0
-colors = ["aqua", "salmon", "aquamarine4", "orchid", "gold", "sienna", "red", "olive", "navy", "maroon", "sandy brown"]
+# colors = ["aqua", "salmon", "aquamarine4", "orchid", "gold", "sienna", "red", "olive", "navy", "maroon", "sandy brown"]
 direction = ["right", "left"]
 directions = [0, 90, 180, 270]
 
 exiting = True
 
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r,g,b
 
 def random_walk():
-    chosen_color = random.choice(colors)
+    # chosen_color = random.choice(colors)
+    chosen_color = random_color()
     path_direction = random.choice(direction)
     heading = random.choice(directions)
+    # squirtle.color(chosen_color)
     squirtle.pencolor(chosen_color)
     squirtle.pensize(10)
     squirtle.setheading(heading)
